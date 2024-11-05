7cad0704d33fd2bb42ed3ac5803bae43c4d2160d
huyvu0607 <vudh1092@ut.edu.vn>
2024-11-05 23:17:42 +0700

    add

---
+++ FengShuiKoi.Reponsitories/Entities/KoiAccount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FengShuiKoi.Reponsitories.Entities;
+
+public partial class KoiAccount
+{
+    public int AccId { get; set; }
+
+    public string Password { get; set; } = null!;
+
+    public string? EmailAddress { get; set; }
+
+    public string Description { get; set; } = null!;
+
+    public int? Role { get; set; }
+
+    public virtual ICollection<UserMember> UserMembers { get; set; } = new List<UserMember>();
+}
---
+++ FengShuiKoi.Reponsitories/Entities/UserMember.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FengShuiKoi.Reponsitories.Entities;
+
+public partial class UserMember
+{
+    public int? Accid { get; set; }
+
+    public string FullName { get; set; } = null!;
+
+    public DateTime? Birthday { get; set; }
+
+    public string? Destiny { get; set; }
+
+    public virtual KoiAccount? Acc { get; set; }
+}
